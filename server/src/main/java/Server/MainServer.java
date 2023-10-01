93ef3fcfd9791707fb8a6ed45e0f5b0fa30aca7c
k4rtowka <k4rtowkayt@gmail.com>
2023-10-01 16:12:29 +0300

    Лаб 7. До рефакторинга сервера и команд.

--- server/src/main/java/Server/MainServer.java
+++ server/src/main/java/Server/MainServer.java
@@ -7,7 +7,7 @@ public class MainServer {
         try {
             final String DB_URL = "jdbc:postgresql://localhost:5432/DBMarines";
             final String DB_USERNAME = "postgres";
-            final String DB_PASSWORD = "";
+            final String DB_PASSWORD = "postgres";
             final int idUser = -1;
             TCPServerMultiThread server = new TCPServerMultiThread(new CollectionManagerToSQL(DB_URL, DB_USERNAME, DB_PASSWORD, idUser), System.in, 8080);
             server.Start();
--- src/main/java/Tests/Common/CommonTest.java
+++ src/main/java/Tests/Common/CommonTest.java
@@ -68,7 +68,7 @@ public class CommonTest {
     protected Thread CreateMultiThreadServerFromSQL(String commands, int port) {
         final String DB_URL = "jdbc:postgresql://localhost:5432/DBMarines";
         final String DB_USERNAME = "postgres";
-        final String DB_PASSWORD = "";
+        final String DB_PASSWORD = "postgres";
         final int idUser = 14;
         Thread serverThread = new Thread(() -> {
             try {
